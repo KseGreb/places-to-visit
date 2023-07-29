b255c157e483712690488ba5053254a1a10c7e71
“KseGreb” <ksegreb@gmail.com>
2023-07-29 14:17:40 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { data } from './data';
 
 function App() {
+
+const [travel, setTravel] = useState(data);
+
+const removePlace =(id)=>{
+  let newTravel = travel.filter(travel => travel.id !==id);
+  setTravel(newTravel)
+}
+
+const [showText, setShowText] = useState(false);
+
+const showTextClick = (travelling) => {
+  travelling.showMore = !travelling.showMore;
+  setShowText(!showText)
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className="App">
+
+    <h1>{travel.length} World’s best places to visit!</h1>
+
+    {travel.map(travelling =>{
+      const {id, name, image, description, source, showMore} = travelling;
+    
+      
+
+      return(
+        <div key={id}>
+          <div className='middle'>
+            <h2>{id} - {name}</h2>
+          </div>
+          <div className='middle'>
+            <img src={image} width="400px" alt='beautiful place'/>
+          </div>
+          <div className='middle'>
+            <p>Source: "{source}"</p>
+          </div>
+          <div className='middle'>
+            <p>{showMore ? description : description.substring(0,50) + "..."} 
+              <button onClick={()=>showTextClick(travelling)} className='btn'>{showMore ? "show less" : "show more"}</button>
+            </p>
+          </div>
+          <div className='middle'>
+            <button onClick={()=>removePlace(id)}>Already visited!</button>
+          </div>
+        </div>
+      )
+    })}
+
+    <button onClick={()=> setTravel ([])}>DELETE ALL</button>
+     
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,50 @@
+export const data = [
+    {
+        id: "1",
+        name: "Bali, Indonesia",
+        image: "https://imageio.forbes.com/specials-images/dam/imageserve/1158376538/960x0.jpg?format=jpg&width=960",
+        description: "You’ll find beaches, volcanoes, Komodo dragons and jungles sheltering elephants, orangutans and tigers. Basically, it’s paradise. ",
+        source: "Forbes",
+        showMore: false
+    },
+    {
+        id: "2",
+        name: "Marrakesh, Morocco",
+        image: "https://imageio.forbes.com/specials-images/dam/imageserve/1166259909/960x0.jpg?format=jpg&width=960",
+        description: "This ancient walled city is home to mosques, palaces and lush gardens. It’s known as The Red City thanks to the color of the brick walls surrounding the city. The medina is a UNESCO World Heritage Centre.",
+        source: "Forbes",
+        showMore: false
+    },
+    {
+        id: "3",
+        name: "Bora Bora, French Polynesia",
+        image: "https://imageio.forbes.com/specials-images/dam/imageserve/1141969923/960x0.jpg?format=jpg&width=960",
+        description: "Bora Bora is Tahiti's most famous island. How do overwater bungalows and underwater adventures sound to you? To us, it’s everything we could want from a bucket list destination.",
+        source: "Forbes",
+        showMore: false
+    },
+    {
+        id: "4",
+        name: "Edinburgh, Scotland",
+        image: "https://imageio.forbes.com/specials-images/dam/imageserve/1167819565/960x0.jpg?format=jpg&width=960",
+        description: "With the historic Edinburgh castle looming over the city, culture in spades and wonderfully friendly locals, this is one of the world’s greatest city breaks.",
+        source: "Forbes",
+        showMore: false
+    },
+    {
+        id: "5",
+        name: "Jaipur, India",
+        image: "https://imageio.forbes.com/specials-images/dam/imageserve/1153530085/960x0.jpg?format=jpg&width=960",
+        description: "Jaipur is known as the ‘Pink City’ for its pale terracotta buildings. This was originally done to impress the visiting Prince Albert during his 1876 tour of India by order of the Maharaja (Sawai Ram Singh). Even today, it’s illegal to paint buildings any other color.",
+        source: "Forbes",
+        showMore: false
+    },
+    {
+        id: "6",
+        name: "Kruger National Park, South Africa",
+        image: "https://imageio.forbes.com/specials-images/dam/imageserve/1161372108/960x0.jpg?format=jpg&width=960",
+        description: "The Kruger National Park is a vast space in northeastern South Africa that is home to a huge array of wildlife. The park has safari experiences of all kinds, allowing you the opportunity to escape busy city living and get back to nature.",
+        source: "Forbes",
+        showMore: false
+    },
+]
